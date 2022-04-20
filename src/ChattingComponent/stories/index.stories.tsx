89d44796f53c3a452372092f1d8fc0fd34c50c46
seokhwandan <dansh87@gmail.com>
2022-04-20 09:28:29 +0900

    sticker chat fix

--- src/ChattingComponent/stories/index.stories.tsx
+++ src/ChattingComponent/stories/index.stories.tsx
@@ -7,5 +7,9 @@ export default {
 
 export const Chatting = () => <ChattingComponent />
 export const CustomChatting = () => (
-  <ChattingComponent width={400} height={600} />
+  <ChattingComponent
+    width={400}
+    height={600}
+    sticker={'https://img.stipop.io/2020/11/22/1606019666828_01.gif'}
+  />
 )
